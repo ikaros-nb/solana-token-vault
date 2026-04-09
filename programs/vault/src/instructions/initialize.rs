c3beb1af3b7dd8f280b4087dd341e862763e5d4e
Nicolas Bouème <nicolasboueme@gmail.com>
2026-04-09 20:07:54 +0900

    Add VaultState account and update Initialize instruction structure

--- programs/vault/src/instructions/initialize.rs
+++ programs/vault/src/instructions/initialize.rs
@@ -1,7 +1,20 @@
 use anchor_lang::prelude::*;
+use crate::VaultState;
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        init, 
+        payer = payer, 
+        space = 8 + VaultState::INIT_SPACE, 
+        seeds = [b"vault", payer.key().as_ref()], 
+        bump
+    )]
+    pub vault: Account<'info, VaultState>,
+    pub system_program: Program<'info, System>,
+}
 
 pub fn handler(ctx: Context<Initialize>) -> Result<()> {
     msg!("Greetings from: {:?}", ctx.program_id);
---
+++ programs/vault/src/state.rs
@@ -0,0 +1,8 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(InitSpace)]
+pub struct VaultState {
+    pub owner: Pubkey,
+    pub bump: u8,
+}
