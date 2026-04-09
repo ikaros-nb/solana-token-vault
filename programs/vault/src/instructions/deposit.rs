5d9b052f27952aa649f9ad26c545f72010b238d5
Nicolas Bouème <nicolasboueme@gmail.com>
2026-04-09 22:30:52 +0900

    Create Deposit structure within its (empty) handler

---
+++ programs/vault/src/instructions/deposit.rs
@@ -0,0 +1,32 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
+
+use crate::VaultState;
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account()]
+    pub vault: Account<'info, VaultState>,
+
+    #[account(constraint = vault.mint == mint.key())]
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(mut)]
+    pub payer_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut, 
+        seeds = [b"token", vault.key().as_ref()],
+        bump
+    )]
+    pub vault_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+pub fn handler(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+    Ok(())
+}
--- programs/vault/src/lib.rs
+++ programs/vault/src/lib.rs
@@ -18,4 +18,8 @@ pub mod vault {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         initialize::handler(ctx)
     }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        deposit::handler(ctx, amount)
+    }
 }
