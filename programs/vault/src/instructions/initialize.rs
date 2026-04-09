3db8140dc2ca9e06f77db8ba70686393fbc9c771
Nicolas Bouème <nicolasboueme@gmail.com>
2026-04-09 20:43:30 +0900

    Add Tokken Account inside Initialize structure

--- programs/vault/src/instructions/initialize.rs
+++ programs/vault/src/instructions/initialize.rs
@@ -1,10 +1,13 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
+
 use crate::VaultState;
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
+
     #[account(
         init, 
         payer = payer, 
@@ -13,6 +16,20 @@ pub struct Initialize<'info> {
         bump
     )]
     pub vault: Account<'info, VaultState>,
+
+    #[account(
+        init,
+        payer = payer,
+        token::mint = mint,
+        token::authority = vault,
+        token::token_program = token_program,
+        seeds = [b"token", vault.key().as_ref()],
+        bump
+    )]
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+    pub token_program: Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
 }
 
